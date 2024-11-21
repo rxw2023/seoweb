0c8bb3930edba429408d2e07f10f3ef69b01cb29
Rao <131958876+rxw2023@users.noreply.github.com>
2024-11-21 13:29:59 +0800

    Update add_post_blog.py

--- add_post_blog.py
+++ add_post_blog.py
@@ -1,78 +1,75 @@
-from bs4 import BeautifulSoup
-from datetime import datetime
+from bs4 import BeautifulSoup  
+from datetime import datetime  
 
-def add_blog_post(title, date, description, link):
-    # Open the blog.html file
-    with open("blog.html", "r", encoding="utf-8") as file:
-        soup = BeautifulSoup(file, "html.parser")
+def add_blog_post(title, date, description, link):  
+    # 打开 blog.html 文件  
+    with open("blog.html", "r", encoding="utf-8") as file:  
+        soup = BeautifulSoup(file, "html.parser")  
     
-    # Find the parent container of the pagination element
-    pagination_container = soup.find("div", class_="pagination")
+    # 找到博客文章容器  
+    blog_posts_container = soup.find("div", class_="blog-posts")  
     
-    # Find the blog post container
-    blog_posts_container = soup.find("div", class_="blog-posts")
+    # 创建新的博客文章元素  
+    new_post = soup.new_tag("div", attrs={"class": "blog-post"})   
     
-    # Create a new blog post element
-    new_post = soup.new_tag("div", attrs={"class": "blog-post"}) 
+    # 创建博客标题  
+    title_tag = soup.new_tag("h3")  
+    title_tag.string = title  
+    new_post.append(title_tag)  
     
-    # Create blog title
-    title_tag = soup.new_tag("h3")
-    title_tag.string = title
-    new_post.append(title_tag)
+    # 创建博客的元数据（日期）  
+    meta_tag = soup.new_tag("div", attrs={"class": "blog-meta"})  
+    date_tag = soup.new_tag("span", attrs={"class": "blog-date"})  
     
-    # Create blog metadata (date)
-    meta_tag = soup.new_tag("div", attrs={"class": "blog-meta"})
-    date_tag = soup.new_tag("span", attrs={"class": "blog-date"})
+    # 创建 <i> 标签并插入日期  
+    i_tag = soup.new_tag("i", attrs={"class": "fas fa-calendar-alt"})  
+    i_tag.string = f" {date}"  
     
-    # Create an <i> tag and insert the date
-    i_tag = soup.new_tag("i", attrs={"class": "fas fa-calendar-alt"})
-    i_tag.string = f" {date}"
+    # 将 <i> 标签添加到日期标签  
+    date_tag.append(i_tag)  
+    meta_tag.append(date_tag)  
+    new_post.append(meta_tag)  
     
-    # Add <i> tag to date tag
-    date_tag.append(i_tag)
-    meta_tag.append(date_tag)
-    new_post.append(meta_tag)
+    # 创建博客描述  
+    description_tag = soup.new_tag("p")  
+    description_tag.string = description  
+    new_post.append(description_tag)  
     
-    # Create a description for your blog
-    description_tag = soup.new_tag("p")
-    description_tag.string = description
-    new_post.append(description_tag)
+    # 创建“阅读更多”链接  
+    read_more_tag = soup.new_tag("div", attrs={"class": "read-more"})  
+    link_tag = soup.new_tag("a", href=link, attrs={"class": "read-more-btn"})  
+    link_tag.string = "Read More"  
+    read_more_tag.append(link_tag)  
+    new_post.append(read_more_tag)  
     
-    # Create a "Read More" link
-    read_more_tag = soup.new_tag("div", attrs={"class": "read-more"})
-    link_tag = soup.new_tag("a", href=link, attrs={"class": "read-more-btn"})
-    link_tag.string = "Read More"
-    read_more_tag.append(link_tag)
-    new_post.append(read_more_tag)
+    # 将新博客文章插入到博客文章容器的顶部  
+    blog_posts_container.insert(0, new_post)  
     
-    # Insert new blog post before pagination element
-    pagination_container.insert_before(new_post)
+    # 也将文章添加到最近博客列表  
+    recent_blogs = soup.find("div", class_="recent-blogs")  
+    recent_blog_list = recent_blogs.find("ul")  
+    recent_blog_item = soup.new_tag("li")  
+    recent_blog_link = soup.new_tag("a", href=link)  
+    recent_blog_link.string = title  
+    recent_blog_item.append(recent_blog_link)  
+    recent_blog_list.append(recent_blog_item)  
     
-    # Also adds the article to the list of recent blogs
-    recent_blogs = soup.find("div", class_="recent-blogs")
-    recent_blog_list = recent_blogs.find("ul")
-    recent_blog_item = soup.new_tag("li")
-    recent_blog_link = soup.new_tag("a", href=link)
-    recent_blog_link.string = title
-    recent_blog_item.append(recent_blog_link)
-    recent_blog_list.append(recent_blog_item)
-    
-    # Write updated HTML back to file
-    with open("blog.html", "w", encoding="utf-8") as file:
-        file.write(str(soup))
+    # 将更新后的 HTML 写回文件中  
+    with open("blog.html", "w", encoding="utf-8") as file:  
+        file.write(str(soup))  
 
-# Input interface
-def get_blog_input():
-    print("Please enter the block information for the blog post：")
-    title = input("Please enter blog title：")
-    date = input(f"Please enter the article date（Default format：{datetime.now().strftime('%Y.%m.%d')}，Press enter to use the default date）：") or datetime.now().strftime("%Y.%m.%d")
-    description = input("Please enter a short description of your blog：")
-    link = input("Please enter the link to the article（For example：post1.html）：")
+# 输入接口  
+def get_blog_input():  
+    print("请输入博客文章的信息：")  
+    title = input("请输入博客标题：")  
+    date = input(f"请输入文章日期（默认格式：{datetime.now().strftime('%Y.%m.%d')}，按回车使用默认日期）：") or datetime.now().strftime("%Y.%m.%d")  
+    description = input("请输入博客的简短描述：")  
+    link = input("请输入文章的链接（例如：post1.html）：")  
     
-    return title, date, description, link
+    return title, date, description, link  
 
-# Get user input
-title, date, description, link = get_blog_input()
+# 获取用户输入  
+title, date, description, link = get_blog_input()  
 
-# Add blog post
+# 添加博客文章  
 add_blog_post(title, date, description, link)
